6152bf8c3fd3eee2d9dca687a9d8e3e34e9f2dd1
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-10-10 22:42:02 -0300

    Mudança visual no programa

--- main.py
+++ main.py
@@ -45,9 +45,8 @@ def entrada_usuario():
                 texto_resumo += entry.text
             prompt_usuario = texto_resumo
             response = model.generate_content(f'Faça um resumo sobre {prompt_usuario}. Na hora da criação do Resumo não utilize ** ou hashtags, Utilize SOMENTE setas ou pequenos pontos brancos flutuantes.')
-            texto_resumo_label = CTkLabel(frame_conteudo, text=response.text, wraplength=600)
+            texto_resumo_label = CTkLabel(frame_conteudo, text=response.text, wraplength=300, text_color='#5c0078')
             texto_resumo_label.pack()
-            frame_gato.destroy()
     botao_inserir.delete(0, END)
 
 # Configuração modelo da AI e KEY
@@ -56,41 +55,42 @@ client = genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 model = genai.GenerativeModel('gemini-2.0-flash-exp')
 
 # Janela Principal
-janela = CTk(fg_color='#1E1E1E')
+janela = CTk(fg_color='#cb6ce6')
 janela.iconbitmap('icone.ico')
 janela.title('Summa')
 janela.resizable(False, False)
 centralizar(700,425, janela)
 
 #Botão inserir conteúdo
-botao_inserir = CTkEntry(janela, placeholder_text='Cole aqui o link do vídeo para resumir 📝', width=500, height=50, fg_color='#333333', border_width=2, border_color='#A78BFA')
-botao_inserir.pack(side='top')
+botao_inserir = CTkEntry(janela, placeholder_text='Cole seu link', width=400, height=40, fg_color='#5c0078', corner_radius=50, border_color='#5c0078')
+botao_inserir.place(relx=0.215, rely=0.18)
 janela.bind('<Return>', lambda event: entrada_usuario())
 
 #Frame scrollavel com resumo
-frame_conteudo = CTkScrollableFrame(janela, width=700, height=300)
-frame_conteudo.pack(side='top', pady=10)
-frame_conteudo.configure(scrollbar_button_color="gray14",scrollbar_button_hover_color="gray14", fg_color='#2c2f33')
-
-img_gato = CTkImage(dark_image=Image.open('gato.png'), size=(350,350))
-frame_gato = CTkLabel(frame_conteudo, image=img_gato, text='')
-frame_gato.pack()
+frame_conteudo = CTkScrollableFrame(janela, width=365, height=0, corner_radius=15, fg_color='#d6b2e0')
+frame_conteudo.place(relx=0.5, rely=0.55, anchor='center')
+frame_conteudo.configure(scrollbar_button_color="#d6b2e0",scrollbar_button_hover_color="#d6b2e0")
 
 #Frame Rodapé
 frame_rodape = CTkFrame(janela, width=700, height=50, fg_color='transparent')
 frame_rodape.pack(side='bottom')
 
 #Botões do Rodapé
-img_copiar = CTkImage(dark_image=Image.open('botao_copiar.png'), size=(25,25))
-botao_copiar = CTkButton(frame_rodape, width=25, height=25, text='Copiar', image=img_copiar, fg_color='#A78BFA', text_color='black', hover_color='#C2A5FF', command= lambda: copiar(response.text))
-botao_copiar.place(relx=0.5, rely=0.45, anchor='center')
+img_copiar = CTkImage(dark_image=Image.open('botao_copiar.png'), size=(35,35))
+botao_copiar = CTkButton(frame_rodape, width=50, height=25, text='Copiar', image=img_copiar, fg_color='white', text_color='black', hover_color='#C2A5FF', command= lambda: copiar(response.text), corner_radius=20)
+botao_copiar.place(relx=0.02, rely=0)
 
 img_github = CTkImage(dark_image=Image.open('github.png'), size=(30, 30))
-botao_github = CTkButton(frame_rodape, width=15, height=15, image=img_github, text='', fg_color='#A78BFA', hover_color='#C2A5FF', corner_radius=50, command= lambda: abrirgithub())
-botao_github.place(relx=0.02, rely=0)
+botao_github = CTkButton(frame_rodape, width=15, height=15, image=img_github, text='', fg_color='white', hover_color='#C2A5FF', corner_radius=50, command= lambda: abrirgithub())
+botao_github.place(relx=0.9, rely=0)
 
-botao_duvida = CTkButton(frame_rodape, text='?', width=40, height=15, font=('comic sans ms', 25), text_color='black', fg_color='#A78BFA', corner_radius=10, hover_color='#C2A5FF', command=lambda: ajuda())
-botao_duvida.place(relx=0.93, rely=0)
+botao_duvida = CTkButton(frame_rodape, text='?', width=40, height=15, font=('comic sans ms', 25), text_color='black', fg_color='white', corner_radius=10, hover_color='#C2A5FF', command=lambda: ajuda())
+botao_duvida.place(relx=0.18, rely=0)
+
+#Logo
+gato_img = CTkImage(dark_image=Image.open('gato_icone.png'), size=(70,70))
+gato_label = CTkLabel(janela, text='S U M M A', image=gato_img, compound='left', font=('Times New Roman ', 30), fg_color='transparent')
+gato_label.place(relx=0, rely=0)
 
 #Texto de Resumo
 texto_resumo_label = CTkLabel(frame_conteudo, text='', wraplength=600)
