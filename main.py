7a569e66d3d93189b5ba5e133b9ff99e3d176721
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-10-10 21:13:32 -0300

    botões funcionais, designer alterado

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # Importações de Librarys e APIs
+import CTkMessagebox
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
 from customtkinter import *
 from PIL import Image
+from CTkMessagebox import ctkmessagebox
+import webbrowser
 
 # Funções
+def abrirgithub():
+    navegador = webbrowser.get()
+    navegador.open('https://github.com/rafssunny')
+def ajuda():
+    CTkMessagebox.CTkMessagebox(title='Ajuda', message='• Utilize links como este: https://www.youtube.com/watch?v=UcLoXF8N_No\n\n• Não use links que incluam o tempo do vídeo no final do link ou que estejam em um formato diferente. Exemplos de links incorretos:\nhttps://www.youtube.com/watch?v=iD5y-oZOFAM&t=132s\nhttps://youtu.be/uenpi3MW8pQ?si=BXIxQAWy2LcuHGgi')
+def copiar(texto):
+    janela.clipboard_clear()
+    janela.clipboard_append(texto)
+    janela.update()
+    CTkMessagebox.CTkMessagebox(title='Copiar texto', message='Copiado com sucesso.', icon='check', option_1='Ok.')
 def centralizar(largura, altura, janela):
     largura_janela = largura
     altura_janela = altura
@@ -16,7 +29,7 @@ def centralizar(largura, altura, janela):
     pos_y = int((altura_tela/2) - int(altura_janela/2))
     janela.geometry(f'{largura_janela}x{altura_janela}+{pos_x}+{pos_y}')
 def entrada_usuario():
-    global texto_resumo_label, frame_conteudo, texto_resumo
+    global texto_resumo_label, frame_conteudo, texto_resumo, response, frame_gato
     texto_resumo = ''
     texto_resumo_label.destroy()
     prompt_usuario = botao_inserir.get()
@@ -25,18 +38,20 @@ def entrada_usuario():
     else:
         #Checagem se é um ID do youtube ou não.
         try:
+            youtube_id = prompt_usuario[32:]
             from youtube_transcript_api import YouTubeTranscriptApi
-            yt = YouTubeTranscriptApi().fetch(prompt_usuario, languages=['pt', 'en'])
+            yt = YouTubeTranscriptApi().fetch(youtube_id, languages=['pt', 'en'])
         except:
-            pass
+            CTkMessagebox.CTkMessagebox(title='ERRO', message='Insira o link do vídeo corretamente.', icon='cancel', option_1='Ok.')
         else:
             for entry in yt:
                 texto_resumo += entry.text
             prompt_usuario = texto_resumo
-        response = model.generate_content(f'Faça um resumo sobre {prompt_usuario}. Na hora da criação do Resumo não utilize asterisco ou hashtags, Utilize SOMENTE setas ou pequenos pontos brancos flutuantes. Caso você receba o link de um vídeo do youtube antes do "Faça um resumo sobre...", você mandará SOMENTE a seguinte frase: "Mande o ID do vídeo, não o link."')
-        texto_resumo_label = CTkLabel(frame_conteudo, text=response.text, wraplength=600)
-        texto_resumo_label.pack()
-        botao_inserir.delete(0, END)
+            response = model.generate_content(f'Faça um resumo sobre {prompt_usuario}. Na hora da criação do Resumo não utilize ** ou hashtags, Utilize SOMENTE setas ou pequenos pontos brancos flutuantes.')
+            texto_resumo_label = CTkLabel(frame_conteudo, text=response.text, wraplength=600)
+            texto_resumo_label.pack()
+            frame_gato.destroy()
+    botao_inserir.delete(0, END)
 
 # Configuração modelo da AI e KEY
 load_dotenv()
@@ -44,20 +59,25 @@ client = genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 model = genai.GenerativeModel('gemini-2.0-flash-exp')
 
 # Janela Principal
-janela = CTk()
+janela = CTk(fg_color='#1E1E1E')
+janela.iconbitmap('icone.ico')
 janela.title('Summa')
 janela.resizable(False, False)
 centralizar(700,425, janela)
 
 #Botão inserir conteúdo
-botao_inserir = CTkEntry(janela, placeholder_text='Insira o assunto ou ID do vídeo para ser resumido...', width=500, height=50)
+botao_inserir = CTkEntry(janela, placeholder_text='Cole aqui o link do vídeo para resumir 📝', width=500, height=50, fg_color='#333333', border_width=2, border_color='#A78BFA')
 botao_inserir.pack(side='top')
 janela.bind('<Return>', lambda event: entrada_usuario())
 
 #Frame scrollavel com resumo
 frame_conteudo = CTkScrollableFrame(janela, width=700, height=300)
 frame_conteudo.pack(side='top', pady=10)
-frame_conteudo.configure(scrollbar_button_color="gray14",scrollbar_button_hover_color="gray14")
+frame_conteudo.configure(scrollbar_button_color="gray14",scrollbar_button_hover_color="gray14", fg_color='#2c2f33')
+
+img_gato = CTkImage(dark_image=Image.open('gato.png'), size=(350,350))
+frame_gato = CTkLabel(frame_conteudo, image=img_gato, text='')
+frame_gato.pack()
 
 #Frame Rodapé
 frame_rodape = CTkFrame(janela, width=700, height=50, fg_color='transparent')
@@ -65,18 +85,15 @@ frame_rodape.pack(side='bottom')
 
 #Botões do Rodapé
 img_copiar = CTkImage(dark_image=Image.open('botao_copiar.png'), size=(25,25))
-botao_copiar = CTkButton(frame_rodape, width=25, height=25, text='Copiar', image=img_copiar, fg_color='white', text_color='black', hover_color='#b5b5b5')
+botao_copiar = CTkButton(frame_rodape, width=25, height=25, text='Copiar', image=img_copiar, fg_color='#A78BFA', text_color='black', hover_color='#C2A5FF', command= lambda: copiar(response.text))
 botao_copiar.place(relx=0.5, rely=0.45, anchor='center')
 
-botao_cor = CTkSwitch(frame_rodape, text='Mudar cor')
-botao_cor.place(relx=0.95, rely=0.5, anchor='e')
-
 img_github = CTkImage(dark_image=Image.open('github.png'), size=(30, 30))
-botao_github = CTkButton(frame_rodape, width=15, height=15, image=img_github, text='', fg_color='white', hover_color='#b5b5b5', corner_radius=50)
+botao_github = CTkButton(frame_rodape, width=15, height=15, image=img_github, text='', fg_color='#A78BFA', hover_color='#C2A5FF', corner_radius=50, command= lambda: abrirgithub())
 botao_github.place(relx=0.02, rely=0)
 
-botao_duvida = CTkButton(frame_rodape, text='?', width=40, height=15, font=('comic sans ms', 25), text_color='black', fg_color='white', corner_radius=10, hover_color='#b5b5b5')
-botao_duvida.place(relx=0.1, rely=0)
+botao_duvida = CTkButton(frame_rodape, text='?', width=40, height=15, font=('comic sans ms', 25), text_color='black', fg_color='#A78BFA', corner_radius=10, hover_color='#C2A5FF', command=lambda: ajuda())
+botao_duvida.place(relx=0.93, rely=0)
 
 #Texto de Resumo
 texto_resumo_label = CTkLabel(frame_conteudo, text='', wraplength=600)
