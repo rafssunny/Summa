4ae68e628b7c75aac00605d70ea29ba70b5e96a9
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-10-16 16:16:01 -0300

    Corrigindo problema de compilação

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 from cx_Freeze import setup, Executable
 
-# Nome e versão do aplicativo
-app_name = "Summa"
-app_version = "1.0"
+if __name__ == "__main__":
+    # Nome e versão do aplicativo
+    app_name = "Summa"
+    app_version = "1.0"
 
-# Arquivos extras que devem ir junto com o executável
-include_files = [
-    "icone.ico",
-    "click.cur",
-    "cursor.cur",
-    "tipo.cur",
-    "imgs/"
-]
-
-# Opções de build (compilação)
-build_exe_options = {
-    "include_files": include_files,
-    "include_msvcr": True,
-}
-
-# Configuração principal
-setup(
-    name=app_name,
-    version=app_version,
-    description="Aplicativo Summa - resumo de vídeos e textos",
-    options={"build_exe": build_exe_options},
-    executables=[
-        Executable(
-            script="main.py",
-            base="Win32GUI",
-            icon="icone.ico"
-        )
+    include_files = [
+        "icone.ico",
+        "click.cur",
+        "cursor.cur",
+        "tipo.cur",
+        "imgs/"
     ]
-)
+
+    build_exe_options = {
+        "include_files": include_files,
+        "include_msvcr": True,
+    }
+
+    setup(
+        name=app_name,
+        version=app_version,
+        description="Aplicativo Summa - resumo de vídeos e textos",
+        options={"build_exe": build_exe_options},
+        executables=[
+            Executable(
+                script="main.py",
+                base="Win32GUI",
+                icon="icone.ico"
+            )
+        ]
+    )
